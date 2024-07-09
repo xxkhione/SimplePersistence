41001632960a20b94eeed86810300e4325c5bd90
EmberXxX <amberoliver2005@gmail.com>
2024-07-08 21:46:40 -0600

    Search files method

---
+++ src/edu/neumont/dbt230/controller/SearchFiles.java
@@ -0,0 +1,21 @@
+package edu.neumont.dbt230.controller;
+
+import java.io.File;
+
+public class SearchFiles {
+
+    private static final File FILE_PATH = new File("C:/Year1-Neumont/Databases 2/AssignmentOneData/people/long");
+
+    private static final int FILE_MAX = 10;
+
+    public static String searchingFiles(int id){
+        for (int i = 0; i <= FILE_MAX ; i++) {
+            if(i == id){
+                return FILE_PATH.toString() + id +".txt";
+            } else {
+                i++;
+            }
+        }
+        return null;
+    }
+}
