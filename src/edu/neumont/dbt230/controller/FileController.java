f5aa49c79b54d43926d3a544e6b9b145b4ea26e5
EmberXxX <amberoliver2005@gmail.com>
2024-07-26 02:37:11 -0600

    Added the Create file

--- src/edu/neumont/dbt230/controller/FileController.java
+++ src/edu/neumont/dbt230/controller/FileController.java
@@ -9,37 +9,79 @@ package edu.neumont.dbt230.controller;
 import edu.neumont.dbt230.view.Display;
 
 public class FileController {
+
     public void run(){
         do{
-            //Welcome the user
-            int selection = -1; //UI method that takes in the user input
+            // Welcome the user
+            Display.welcomeMsg();
+            int selection = Display.mainMenu(); // UI method that takes in the user input
             switch(selection)
             {
-                case 1: //1. Create an employee file
-                    //get the first name
-                    //get the last name
-                    //get the hire year
-                    //calls the formating file
-                    //calls createFile
+                case 1:
+                    // 1. Create an employee file
+                        // get the first name
+                        // get the last name
+                        // get the hire year
+                        // calls the formating file
+                        // calls createFile
+
+                    String fName = Display.getFirstName();
+                    String lName = Display.getLastName();
+                    int hireYear = Display.getHireYear();
+
+                    FileManipulator.createFile(formatEmployeeContents(fName, lName, hireYear));
+
                     break;
-                case 2: //2. Update an employee file
-                    //get the id of the employee the user wants to update
-                    //get the selection of what they want to update
-                    //get the info they want to update
-                    //calls updateFile
+                case 2:
+                    // 2. Update an employee file
+                        // get the id of the employee the user wants to update
+                        // get the selection of what they want to update
+                        // get the info they want to update
+                        // calls updateFile
+
+                    Display.getIDSearch();
+                        int option = Display.updateMenu();
+                        switch(option){
+                            case 1:
+                                // First Name
+                                Display.updateFirstName();
+                                break;
+                            case 2:
+                                // Last Name
+                                Display.updateLastName();
+                                break;
+                            case 3:
+                                // Hire Year
+                                Display.updateHireYear();
+                                break;
+                        }
+
                     break;
-                case 3: //3. Delete an employee file
-                    //get the id of the employee the user wants to delete
-                    //calls deleteFile
+                case 3:
+                    // 3. Delete an employee file
+                        // get the id of the employee the user wants to delete
+                        // calls deleteFile
+
+                    int idDelete = Display.getIDSearch();
+                    FileManipulator.deleteFile(idDelete);
+
                     break;
-                case 4: //4. Search for an employee
-                    //get the id of the employee the user wants to find
-                    //calls getSingleEmployee
+                case 4:
+                    // 4. Search for an employee
+                        // get the id of the employee the user wants to find
+                        // calls getSingleEmployee
+
+                    int idSearch = Display.getIDSearch();
+                    FileManipulator.deleteFile(idSearch);
+
                     break;
-                case 5: //5. View all employees
+                case 5:
+                    // 5. View all employees
                     Display.printEmployees();
                     break;
-                default: //6. Quit
+                default:
+                    // 6. Quit
+                    Display.quit();
                     return;
             }
         } while(true);
--- src/edu/neumont/dbt230/view/Display.java
+++ src/edu/neumont/dbt230/view/Display.java
@@ -10,6 +10,82 @@ import edu.neumont.dbt230.controller.FileManipulator;
 import edu.neumont.dbt230.model.Employee;
 
 public class Display {
+
+    static Console console = new Console();
+
+    // Welcome the user (void)
+    public static void welcomeMsg(){
+        System.out.println("Welcome to the Employee Database!");
+    }
+
+    // Console menu (returns an int)
+    public static int mainMenu(){
+        int option = console.getIntInput("1. Create Employee File \n2. Update Employee File \n3. Delete Employee File \n4. Search for an Employee File \n" +
+                "5. View All Employees \n6. Exit");
+        return option;
+    }
+
+    // Get first name of employee (returns a string)
+    public static String getFirstName(){
+        String firstName = console.getStringInput("Enter the Employee's first name: ");
+        return firstName;
+    }
+
+    // Get last name of employee (returns a string)
+    public static String getLastName(){
+        String lastName = console.getStringInput("Enter the Employee's last name: ");
+        return lastName;
+    }
+
+    // Get hire year of employee (returns an int)
+    public static int getHireYear(){
+        int hireYear = console.getIntInput("Enter the Employee's hire year: ");
+        return hireYear;
+    }
+
+    // Get the id they want to search/update (returns an int)
+    public static int getIDSearch(){
+        int idSearch = console.getIntInput("Enter the id of the wanted Employee: ");
+        return idSearch;
+    }
+
+    // menu for getting what the user would like to update (returns an int)
+    public static int updateMenu(){
+        int option = console.getIntInput("Choose what you would like to update:\n 1. First Name\n 2. Last Name\n 3. Hire Year");
+        return option;
+    }
+
+    // Get what value the user wants to update (firstName, lastName, or hireYear) (returns an int)
+    public static String updateFirstName(){
+        String firstName = console.getStringInput("Enter the Employee's updated first name: ");
+        return firstName;
+    }
+
+    public static String updateLastName(){
+        String lastName = console.getStringInput("Enter the Employee's updated last name: ");
+        return lastName;
+    }
+
+    public static int updateHireYear(){
+        int hireYear = console.getIntInput("Enter the Employee's updated hire year: ");
+        return hireYear;
+    }
+
+    public static void quit(){
+        Console.getStringInput("Exiting Application..... Press any key to exit.");
+    }
+
+    // Successful message (void)
+    public static void successfulMsg(){
+        System.out.println("Success!");
+    }
+
+    // Error message (for when an employee doesn't exist) (void)
+
+
+
+    // Print a single employee (void)
+
     //Print the structured employees
     public static void printEmployees(){
         System.out.println("Printing structured employees:");
@@ -18,15 +94,5 @@ public class Display {
             System.out.println("------------------");
         }
     }
-    //Console menu (returns an int)
-    //Get first name of employee (returns a string)
-    //Get last name of employee (returns a string)
-    //Get hire year of employee (returns an int)
-    //Get the id they want to search/update (returns an int)
-    //menu for getting what the user would like to update (returns an int)
-    //Get what value the user wants to update (firstName, lastName, or hireYear) (returns an int)
-    //Successful message (void)
-    //Error message (for when an employee doesn't exist) (void)
-    //Print a single employee (void)
-    //Welcome the user (void)
+
 }
